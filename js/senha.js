ffe901180dafc721ab415c6829f88e84874d9e5c
Jefferson Carlos Lima <92160349+JeffersonCarlosLima@users.noreply.github.com>
2022-05-19 17:39:25 -0300

    Add files via upload

--- js/senha.js
+++ js/senha.js
@@ -4,25 +4,27 @@ const mes = String(dataHora.getMonth() + 1).padStart(2, "0")
 const ano = dataHora.getFullYear()
 const hora = dataHora.getHours()
 let senha = ((dia * ano) - mes) * hora;
-console.log(senha)
+
+
+setTimeout(gerarsenha, 3000)
 
 function gerarsenha() {
     document.getElementById('senha').innerHTML = senha;
 }
 
-copiarSenha = senha;
+//copiarSenha = senha;
 
-function copiarTexto() {
-    /* Get the text field */
-    var copyText = copiarSenha;
+//function copiarTexto() {
+/* Get the text field */
+// var copyText = copiarSenha;
 
-    /* Select the text field */
-    copyText.select();
-    copyText.setSelectionRange(0, 99999); /* For mobile devices */
+/* Select the text field */
+// copyText.select();
+// copyText.setSelectionRange(0, 99999); /* For mobile devices */
 
-    /* Copy the text inside the text field */
-    navigator.clipboard.writeText(copyText.value);
+/* Copy the text inside the text field */
+//  navigator.clipboard.writeText(copyText.value);
 
-    /* Alert the copied text */
-    alert("Texto copiado com sucesso!: " + copyText.value);
-}
+/* Alert the copied text */
+// alert("Texto copiado com sucesso!: " + copyText.value);
+//}
